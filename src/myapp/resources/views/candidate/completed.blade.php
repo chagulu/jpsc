040e7e11d74b30031faaa99477138a30c06cfd44
Basanta Swain <basantswainn@gmail.com>
2025-09-12 19:07:33 +0530

    candidate login

--- src/myapp/resources/views/candidate/completed.blade.php
+++ src/myapp/resources/views/candidate/completed.blade.php
@@ -4,11 +4,8 @@
 
 @section('content')
 <div class="d-flex" id="wrapper">
-    
-
     <!-- Main Page -->
     <div class="flex-grow-1 d-flex flex-column">
-     
 
         <!-- Progress Bar -->
         <div class="container-fluid">
@@ -28,23 +25,35 @@
                 <div class="card-body">
                     <i class="fas fa-check-circle text-success" style="font-size: 80px;"></i>
                     <h3 class="mt-3 text-success">Application Submitted Successfully!</h3>
-                    <p class="mt-2">Your application has been successfully submitted to the Jharkhand Public Service Commission.</p>
+                    <p class="mt-2">
+                        Your application has been successfully submitted 
+                        @if($application->submitted_at)
+                            on <strong>{{ $application->submitted_at->format('d-M-Y h:i A') }}</strong>.
+                        @else
+                            to the Commission.
+                        @endif
+                    </p>
                     
                     <div class="alert alert-info mt-4">
-                        <strong>Application ID:</strong> JPSC2025-123456 <br>
-                        <strong>Date:</strong> {{ date('d-M-Y') }}
+                        <strong>Application ID:</strong> {{ $application->application_no ?? 'N/A' }} <br>
+                        <strong>Date:</strong> {{ $application->submitted_at ? $application->submitted_at->format('d-M-Y') : now()->format('d-M-Y') }}
                     </div>
 
                     <div class="mt-4">
-                        <a href="" class="btn btn-outline-primary"><i class="fas fa-eye mr-1"></i> View Application</a>
-                        <a href="#" class="btn btn-success"><i class="fas fa-download mr-1"></i> Download PDF</a>
-                        <a href="#" class="btn btn-warning"><i class="fas fa-print mr-1"></i> Print</a>
+                        <a href="" class="btn btn-outline-primary">
+                            <i class="fas fa-eye mr-1"></i> View Application
+                        </a>
+                        <a href="" class="btn btn-success">
+                            <i class="fas fa-download mr-1"></i> Download PDF
+                        </a>
+                        <a href="" target="_blank" class="btn btn-warning">
+                            <i class="fas fa-print mr-1"></i> Print
+                        </a>
                     </div>
                 </div>
             </div>
         </div>
 
-        
     </div>
 </div>
 
